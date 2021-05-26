292d453df02b6f658135f6f1dc4a430a6be41c46
JavierDHG <42222973+JavierDHG@users.noreply.github.com>
2021-05-26 18:28:14 -0500

    CrearUsuario

---
+++ .github/workflows/ValidarFormulario.php
@@ -0,0 +1,12 @@
+name: Formulario.php
+
+<?php
+echo "Bienvenido";
+$nombre = $_POST['usuario'];
+$clave = $_POST['clave'];
+mysqli_connect("localhost","root","");// primero el host, segundo el user, tercero la contraseña
+mysqli_select_db("Tienda");// hay ke seleccionar la base de datos o si no nunca te va a guardar
+mysqli_query("INSERT INTO sesion(Nombre,Clave) VALUES ('$nombre','$clave')");
+
+?php>
+
